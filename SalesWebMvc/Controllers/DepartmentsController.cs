9f774cc679c2bea1a60ef7501fbdd1cdcdc3c290
Andre Franco <agsfmail@gmail.com>
2026-04-09 17:02:29 -0300

    First MVC - Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>
+            {
+                new Department { Id = 1, Name = "Computers" },
+                new Department { Id = 2, Name = "Electronics" },
+                new Department { Id = 3, Name = "Fashion" },
+                new Department { Id = 4, Name = "Books" }
+            };
+            return View(list);
+        }
+    }
+}
