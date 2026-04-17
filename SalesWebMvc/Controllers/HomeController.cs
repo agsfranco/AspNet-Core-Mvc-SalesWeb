2cb2cc12897dba882c78145d81c508cd05d3a08e
Andre Franco <agsfmail@gmail.com>
2026-04-17 11:28:26 -0300

    Adjustments_3

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace SalesWebMvc.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
